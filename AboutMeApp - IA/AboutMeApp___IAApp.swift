f653a454225f505de705608ac6b47205193a6721
Avanti Singh <avantisingh19@gmail.com>
2025-04-13 15:48:43 -0500

    Added background, created Vstack with a title, image, and image description.

--- AboutMeApp - IA/AboutMeApp___IAApp.swift
+++ AboutMeApp - IA/AboutMeApp___IAApp.swift
@@ -3,7 +3,7 @@
 import SwiftUI
 
 @main
-struct ___PACKAGENAME:identifier___App: App {
+struct AboutMeApp_IA: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
--- AboutMeApp - IA/ContentView.swift
+++ AboutMeApp - IA/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Info = ""
+    @State private var Info2 = ""
+    @State private var Info3 = ""
+    @State private var Info4 = ""
+    @State private var Info5 = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+                   // Color(.sRGB, red: 0.51, green: 0.62, blue: 0.60)
+                    Color(.sRGB, red: 0.0, green: 0.42745098, blue: 0.46666667)
+                        .ignoresSafeArea()
+                   
+                    VStack(spacing: 20) {
+                        Text("About Avanti!")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(.sRGB, red: 0.92941176, green: 0.96470588, blue: 0.97647059))
+                        ZStack{
+                            VStack(spacing: 15){
+                                Image("IMG_5695")
+                                    .resizable(resizingMode: .stretch)
+                                    .aspectRatio(contentMode: .fit)
+                                    .cornerRadius(15)
+                                    
+                                Text("I currently go to the University of Illinois! This picture is my friends and I at an Illinois basketball game.")
+                                    .fontWeight(.medium)
+                                    .multilineTextAlignment(.leading)
+                                
+                                
+                            }
+                            .padding()
+                            .background(Rectangle().foregroundColor(.white))
+                                .cornerRadius(15)
+                                .shadow(radius: 15)
+                                .padding()
+                                
+                            
+                        }
+                        
+                        
+                        
+                    }
+                
+                    
+                    
+                }
     }
 }
 
